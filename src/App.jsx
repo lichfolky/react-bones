6655dafde36709abe6176ba9c1cde8492e30ad78
Lichfolky <lichfolky@gmail.com>
2023-01-03 22:23:30 +0100

    contact card

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import styled from 'styled-components/macro';
 import Quote from './components/quote';
 import FrequentlyAskedQuestion from './components/frequenty-asked-question';
 import LoginForm from './components/login-form';
+import ContactCard from './components/contact-card';
+import cat from './img/cat-300.jpg';
 
 function App() {
   return (
@@ -19,13 +21,31 @@ function App() {
             alert('Submitted!');
           }}
         />
+        <ContactCardWrapper>
+          <ContactCard
+            avatarSrc={cat}
+            name="Mittens"
+            email="meow@gmail.com"
+          />
+        </ContactCardWrapper>
       </Wrapper>
+
     </div>
   );
 }
 
 const Wrapper = styled.main`
     display: grid;
+    grid-template-columns: auto;
     gap: 1rem;
 `;
+
+const ContactCardWrapper = styled.main`
+    display: flex;
+    padding: 5rem 0;
+    width: 100%;
+    background: slateblue;
+    align-items: center;
+`;
+
 export default App;
